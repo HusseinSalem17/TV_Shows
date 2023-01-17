f6dcfcae7eeffb7f0b21f86f36ff15a2ab8de36a
hussein-salemm <husseinsalem177@gmail.com>
2023-01-17 23:02:29 +0200

    .

--- app/src/main/java/com/hussein/tvshows/activities/TVShowDetailsActivity.java
+++ app/src/main/java/com/hussein/tvshows/activities/TVShowDetailsActivity.java
@@ -1,12 +1,16 @@
 package com.hussein.tvshows.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.hussein.tvshows.R;
 import com.hussein.tvshows.adapters.imageSliderAdapter;
@@ -17,7 +21,9 @@ import com.hussein.tvshows.viewmodels.TVShowDetailsViewModel;
 1. doInitialization & getTVShowDetails
 2. Edit on getTVShowDetails after making imageSliderAdapter
 3. make function (loadImageSlider)
-
+4. make function (setupSliderIndicators) & setCurrentSliderIndicator
+5. edit on doInitialization() & getTVShowDetails()
+6. make loadBasicTVSHowDetails()
 
 */
 
@@ -36,6 +42,7 @@ public class TVShowDetailsActivity extends AppCompatActivity {
 
     private void doInitialization() {
         tvShowDetailsViewModel = new ViewModelProvider(this).get(TVShowDetailsViewModel.class);
+        activityTvshowDetailsBinding.imageBack.setOnClickListener(v -> onBackPressed());
         getTVShowDetails();
 
     }
@@ -50,6 +57,11 @@ public class TVShowDetailsActivity extends AppCompatActivity {
                         if (tvShowDetailsResponse.getTvShowDetails().getPictures() != null) {
                             loadImageSlider(tvShowDetailsResponse.getTvShowDetails().getPictures());
                         }
+                        activityTvshowDetailsBinding.setTvShowImageURL(
+                                tvShowDetailsResponse.getTvShowDetails().getImagePath()
+                        );
+                        activityTvshowDetailsBinding.imageTVShow.setVisibility(View.VISIBLE);
+                        loadBasicTVSHowDetails();
                     }
 
                 }
@@ -61,6 +73,63 @@ public class TVShowDetailsActivity extends AppCompatActivity {
         activityTvshowDetailsBinding.sliderViewPager.setAdapter(new imageSliderAdapter(sliderImages));
         activityTvshowDetailsBinding.sliderViewPager.setVisibility(View.VISIBLE);
         activityTvshowDetailsBinding.viewFadingEdge.setVisibility(View.VISIBLE);
+        setupSliderIndicators(sliderImages.length);
     }
 
+    //to make indicator for each image when move to it
+    private void setupSliderIndicators(int count) {
+        ImageView[] indicators = new ImageView[count];
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT
+        );
+        layoutParams.setMargins(8, 0, 8, 0);
+        for (int i = 0; i < indicators.length; i++) {
+            indicators[i] = new ImageView(getApplicationContext());
+            indicators[i].setImageDrawable(ContextCompat.getDrawable(
+                    getApplicationContext(),
+                    R.drawable.background_slider_indicator_inactive
+            ));
+            indicators[i].setLayoutParams(layoutParams);
+            activityTvshowDetailsBinding.layoutSliderIndicators.addView(indicators[i]);
+        }
+        activityTvshowDetailsBinding.layoutSliderIndicators.setVisibility(View.VISIBLE);
+        setCurrentSliderIndicator(0);   //because of first image
+        activityTvshowDetailsBinding.sliderViewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                setCurrentSliderIndicator(position);
+            }
+        });
+
+    }
+
+    private void setCurrentSliderIndicator(int position) {
+        int childCount = activityTvshowDetailsBinding.layoutSliderIndicators.getChildCount();
+        for (int i = 0; i < childCount; i++) {  //time: O(n)
+            ImageView imageView = (ImageView) activityTvshowDetailsBinding.layoutSliderIndicators.getChildAt(i);
+            if (i == position) {
+                imageView.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.background_slider_indicator_active));
+            } else {
+                imageView.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.background_slider_indicator_inactive));
+
+            }
+        }
+
+    }
+
+    private void loadBasicTVSHowDetails() {
+        activityTvshowDetailsBinding.setTvShowName(getIntent().getStringExtra("name"));
+        activityTvshowDetailsBinding.setNetworkCountry(
+                getIntent().getStringExtra("network") + " (" +
+                        getIntent().getStringExtra("country") + ")"
+        );
+        activityTvshowDetailsBinding.setStatus(getIntent().getStringExtra("status"));
+        activityTvshowDetailsBinding.setStartedDate(getIntent().getStringExtra("startDate"));
+        activityTvshowDetailsBinding.textName.setVisibility(View.VISIBLE);
+        activityTvshowDetailsBinding.textNetworkCountry.setVisibility(View.VISIBLE);
+        activityTvshowDetailsBinding.textStatus.setVisibility(View.VISIBLE);
+        activityTvshowDetailsBinding.textStarted.setVisibility(View.VISIBLE);
+
+    }
 }
